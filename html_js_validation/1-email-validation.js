6800c30fc25354fe62dc0b8e47dc0b9ac8e7e665
Adelahpaul <pauladelah1@gmail.com>
2023-10-02 23:29:59 +0100

    task 2 done

---
+++ html_js_validation/1-email-validation.js
@@ -0,0 +1,22 @@
+var emaileror = document.getElementById('error')
+
+function validateEmail(){
+
+    var email = document.getElementById('email').value;
+    if (email.length == 0){
+        emaileror.innerHTML = "Email must be entered";
+        return false;
+    }
+    if(!email.match(/^[A-Za-z]\_.\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,3}$/)){
+        emaileror.innerHTML= "Please enter a valid email address";
+        return false;
+    }
+    return true;
+    form.addEventListener('submit', function (e) {
+        // prevent the form from submitting
+        e.preventDefault();
+        // let checkEmail = validateEmail();
+    });
+    }
+   
+    
